ff75aff9ab82d20986e304b6fb4a9dea4fdce599
kashikaadarsh <45759986+kashikaadarsh@users.noreply.github.com>
2021-04-07 16:28:53 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,125 @@
+const express=require('express');
+const app = express();
+const dbService = require('./dbService');
+const passport = require("passport");
+const session = require("express-session");
+const flash = require("express-flash");
+const bodyParser = require("body-parser");
+const initializePassport = require("./passportConfig");
+initializePassport(passport);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(
+  session({
+    // Key we want to keep secret which will encrypt all of our information
+    secret: 'Cat',
+    // Should we resave our session variables if nothing has changes which we dont
+    resave: false,
+    // Save empty value if there is no vaue which we do not want to do
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/views'));
+
+
+app.get("/",checkAuthenticated,(req,res)=>{
+    res.redirect("/users/login");
+});
+//checkAuthenticated,
+app.get("/users/login",  (req, res) => {
+    // flash sets a messages variable. passport sets the error message
+    // console.log(req.session.flash.error);
+    res.render("login.ejs");
+  });
+  
+app.get("/users/register", checkAuthenticated, (req, res) => {
+    res.render("register.ejs");
+  });
+  
+// app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+//     res.render("dashboard", { user: req.user });
+//   });
+  
+// app.get('/getAll', (request, response) => {
+//     const db = dbService.getDbServiceInstance();
+  
+//     const result = db.getAllData();
+//     result
+//     .then(data => {
+//             response.json({data : data})
+//         })
+//     .catch(err => console.log(err));
+//   });
+  
+app.post('/users/register', (request,response) => {
+    const db = dbService.getDbServiceInstance();
+    const { username,email,password}= request.body;
+    // console.log(username,email,password,plan);
+    const result = db.registerUser(username,email,password);
+    result.then(data => {
+            response.json({data : data})
+        })
+    .catch(err => console.log(err));
+  })
+  
+  // app.post(
+  //   "/users/login",
+  //   passport.authenticate("local", {
+  //     successRedirect: "/users/dashboard",
+  //     failureRedirect: "/users/login",
+  //     failureFlash: true
+  //   })
+  // );
+  
+  // app.get("/users/logout", (req, res) => {
+  //   req.logout();
+  //   res.render("index", { message: "You have logged out successfully" });
+  // });
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/users/dashboard");
+    }
+    next();
+  }
+  
+  // function checkNotAuthenticated(req, res, next) {
+  //   if (req.isAuthenticated()) {
+  //     return next();
+  //   }
+  //   res.redirect("/users/login");
+  // }
+  
+  
+  // Adding new visitor
+  
+  // app.get('/addvisitor/:ip',(request,response) => {
+  //   const db = dbService.getDbServiceInstance();
+  //   const result = db.addVisitor(request.params.ip);
+  //   result.then(() => {
+  //           response.json({data : 'ok'});
+  //       })
+  //   .catch(err => console.log(err));
+  // })
+  
+  
+  
+  
+  
+
+
+
+
+
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
+
+
---
+++ dbService.js
@@ -0,0 +1,236 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'bziqsnahwd71ifrem9c1-mysql.services.clever-cloud.com',
+  user     : 'uek2htonqozkp6pn',
+  password : 'dOOeQCz20bUWO0pT9YCA',
+  database : 'bziqsnahwd71ifrem9c1'
+});
+
+let instance = null;
+
+connection.connect((err)=>{
+  if(!err)
+  console.log("Connection successful !!");
+  else
+  console.log("Connection failed !!");
+});
+
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+
+class dbService {
+
+  static getDbServiceInstance() {
+      return instance ? instance : new dbService();
+  }
+  async searchHospital(name) {
+    try {
+         console.log(name);
+        const response = await new Promise((resolve, reject) => {
+            const query = "SELECT * FROM hospitalInfo WHERE hospitalName= ?";
+            //console.log(query);
+            connection.query(query,[name], (err, results) => {
+                if (err) reject(new Error(err.message));
+                resolve(results);
+            })
+        });
+        //console.log(response)
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  async registerUser(username,email,password) {
+    try {
+        const insertId = await new Promise((resolve, reject) => {
+            
+            const query = "INSERT INTO users (username, email, password) VALUES (?,?,?);";
+            connection.query(query, [username,email,password] , (err, result) => {
+                if (err) reject(new Error(err.message));
+                resolve(result.insertId);
+            })
+        });
+        // console.log(insertId,Name,Numb);
+        return {
+            id : insertId,
+            Name : username,
+            Email : email
+        };
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async authUser(email) {
+    try {
+        const user = await new Promise((resolve, reject) => {
+            const query = "SELECT * FROM users WHERE email = ?";
+            connection.query(query, [email] , (err, result) => {
+                if (err) reject(new Error(err.message));
+                resolve(result);
+            })
+        });
+        if(user.length==0)
+        {
+            return {
+                userFound : false
+            };
+        }
+        return {
+            userFound : true,
+            username : user[0].username,
+            password : user[0].password,
+            id : user[0].id
+        };
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// async deserializeUser(id) {
+//     try {
+//         const user = await new Promise((resolve, reject) => {
+//             const query = "SELECT * FROM users WHERE id = ?";
+//             connection.query(query, [id] , (err, result) => {
+//                 if (err) reject(new Error(err.message));
+//                 resolve(result);
+//             })
+//         });
+//         if(user.length==0)
+//         {
+//             return {
+//                 userFound : false
+//             };
+//         }
+//         return user;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+async getAllData() {
+    try {
+        const response = await new Promise((resolve, reject) => {
+            const query = "SELECT * FROM hospitalInfo;";
+            connection.query(query, (err, results) => {
+                if (err) reject(new Error(err.message));
+                resolve(results);
+            })
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+// async insertNewName(Name,Numb) {
+//     try {
+//         const insertId = await new Promise((resolve, reject) => {
+//             const query = "INSERT INTO mySampleTable (Name,Numb) VALUES (?,?);";
+//             connection.query(query, [Name,Numb] , (err, result) => {
+//                 if (err) reject(new Error(err.message));
+//                 resolve(result.insertId);
+//             })
+//         });
+//         return {
+//             id : insertId,
+//             Name : Name,
+//             Numb : Numb
+//         };
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// async deleteRowById(id) {
+//     try {
+//         id = parseInt(id, 10); 
+//         const response = await new Promise((resolve, reject) => {
+//             const query = "DELETE FROM mySampleTable WHERE id = ?";
+
+//             connection.query(query, [id] , (err, result) => {
+//                 if (err) reject(new Error(err.message));
+//                 resolve(result.affectedRows);
+//             })
+//         });
+
+//         return response === 1 ? true : false;
+//     } catch (error) {
+//         console.log(error);
+//         return false;
+//     }
+// }
+
+// async updateNameById(id, Name) {
+//     try {
+//         id = parseInt(id, 10); 
+//         const response = await new Promise((resolve, reject) => {
+//             const query = "UPDATE mySampleTable SET Name = ? WHERE id = ?";
+
+//             connection.query(query, [Name, id] , (err, result) => {
+//                 if (err) reject(new Error(err.message));
+//                 resolve(result.affectedRows);
+//             })
+//         });
+
+//         return response === 1 ? true : false;
+//     } catch (error) {
+//         console.log(error);
+//         return false;
+//     }
+// }
+
+// async searchByName(Name) {
+//     try {
+//         const response = await new Promise((resolve, reject) => {
+//             const query = "SELECT * FROM mySampleTable WHERE Name = ?;";
+
+//             connection.query(query, [Name], (err, results) => {
+//                 if (err) 
+//                 {
+//                     reject(new Error(err.message));
+//                 }
+//                 resolve(results);
+//             })
+//         });
+
+//         return response;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// async addVisitor(ip) {
+//     try {
+//         const response = await new Promise((resolve, reject) => {
+//             const query = "SELECT * FROM visits WHERE ip= ?;";
+//             connection.query(query,[ip], (err, results) => {
+//                 if (err) reject(new Error(err.message));
+//                 if(results.length)
+//                 {
+//                     const sql = "UPDATE visits SET count = count + 1 WHERE  ip = ?";
+//                     connection.query(sql, [ip], (err, results) => {})
+//                 }
+//                 else
+//                 {
+//                     const query = "INSERT INTO visits (ip) VALUES (?);";
+//                     connection.query(query, [ip], (err, results) => {})
+//                 }
+//             })
+//         });
+//         return 'ok';
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+};
+
+
+//connection.end();
+
+module.exports = dbService;
